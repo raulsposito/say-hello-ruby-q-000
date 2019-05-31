b7693d9f7028892db39e9fdaaf882f72f4b2769d
Raul Sposito <raulsposito@gmail.com>
2019-05-31 02:30:13 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -5,3 +5,7 @@ say_hello("gabriela")
 def say_hello(gabriela, ruby programmer"Gabriela")
   puts "Hello #{name}!"
 end
+
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
